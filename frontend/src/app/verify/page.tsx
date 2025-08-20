959db926f7f45cc45eede74100948930f3dc7828
Biku2004 <bikash.behera24.04.2004@gmail.com>
2025-08-20 22:27:44 +0530

    added frontend

---
+++ frontend/src/app/verify/page.tsx
@@ -0,0 +1,112 @@
+"user client"
+
+import { ArrowRight, Loader2, Lock } from 'lucide-react';
+import { useSearchParams } from 'next/navigation';
+import React, { useEffect, useRef, useState } from 'react'
+
+const page = () => {
+
+  const [loading,setLoading] = useState(false);
+  const [Otp, setOtp] = useState<string[]>(["","","","","","",""]);
+  const [error, setError] = useState<string>("")
+  const [resendLoading,setResendLoading] = useState(false);
+  const [timer, setTimer] = useState(60);
+  const inputRefs = useRef<Array<HTMLInputElement>>([]);
+
+  const searchParams = useSearchParams();
+  const email:string = searchParams.get("email") || "";
+
+  useEffect( ()=> {
+    if(timer > 0){
+      const interval = setInterval(()=>{
+        setTimer((prev)=>prev-1)
+      },1000);
+
+      return ()=> clearInterval(interval)
+    }
+  },[timer]);
+
+  // console.log(timer);
+
+  const handleInputChange = (index:number, value:string):void => {
+    if(value.length > 1) return;
+
+    const newOtp = [...Otp]
+    newOtp[index] = value;
+    setOtp(newOtp);
+    setError("");
+
+    if(value && index < 5){
+      inputRefs.current[index+1]?.focus();
+    }
+  };
+
+  const handleKeyDown = (index:number,e: React.FormEvent<HTMLElement>):void => {
+    if(e.key === "Backspace")
+  }
+
+  const handleSubmit = async () => {}
+
+    return (
+      <>
+        <div className="min-h-screen bg-gray-900 flex items-center justify-center p-4">
+            <div className='max-w-md w-full'>
+                <div className='bg-gray-800 border-gray-700 rounded-lg p-8'>
+                    
+                    <div className='text-center mb-8'>
+                        
+                        <div className="mx-auto w-20 h-20 bg-blue-600 rounded-lg flex items-center justify-center mb-6">
+                            <Lock size={40} className='text-white' />
+                        </div>
+                        <h1 className='text-4xl fondt-bold text-white mb-3'>
+                            Verify Your Email
+                        </h1>
+                        <p className='text-gray-300 text-lg'>
+                            we have sent a 6-digit-code to 
+                        </p>
+                        <p className='text-blue-400 font-medium'>{email}</p>
+                    </div>
+
+                    <form className='space-y-6' onSubmit={handleSubmit}>
+                        <div>
+                            <label htmlFor='email' className='block text-sm font-medium text-gray-300 mb-2'>
+                                Email Address
+                            </label>
+                            <input type="email" id='email' className='w-full px-4 py-4 bg-gray-700
+                             border-gray-600 rounded-lg text-white placeholder-gray-400' 
+                             placeholder='Enter your Email Address' 
+                            //  value={email}
+                            //  onChange={(e) => setEmail(e.target.value)}
+                             required/>
+                        </div>
+                        <button type='submit' className='w-full bg-blue-600 text-white py-4
+                         px-6 rounded-lg font-semibold hover:bg-blue-700 disabled:opacity-50 
+                         disabled:cursor-not-allowed' disabled={loading}>
+                            {
+                                loading ? (
+                                    <div className='flex items-center justify-center gap-2'>
+                                        <Loader2 className='w-5 h-5' />
+                                        Verifying...
+                                    </div>
+                                ):(
+                                    <div className='flex items-center justify-center gap-2'>
+                                        <span>Verify</span>
+                                        <ArrowRight  className='w-5 h-5' />
+                                    </div>
+                                )
+                            }
+                        </button>
+
+                    </form>
+
+
+
+                </div>
+            </div>
+        </div>  
+
+      </>
+    )
+}
+
+export default page
